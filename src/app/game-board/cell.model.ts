aac80acfdf661c1d3340d3af5e292ad14727b634
Niccolo Bellaccini <nicco.b.93@gmail.com>
2019-09-05 15:20:48 +0200

    add other comments

--- src/app/game-board/cell.model.ts
+++ src/app/game-board/cell.model.ts
@@ -39,42 +39,77 @@ export class Cell {
     this.adjMines = adjMines;
   }
 
+  /**
+   * Get the position of the cell
+   */
   getPosition() {
     return this.position;
   }
 
+  /**
+   * Set the position of the cell
+   * @param position the new cell position
+   */
   setPosition(position) {
     this.position = position;
   }
 
+  /**
+   * Return true if the cell is a mine
+   */
   getIsMine() {
     return this.isMine;
   }
 
+  /**
+   * Set if the cell is a mine or not
+   * @param isMine
+   */
   setIsMine(isMine) {
     this.isMine = isMine;
   }
 
+  /**
+   * Return true if the cell is a flag
+   */
   getIsFlag() {
     return this.isFlag;
   }
 
+  /**
+   * Set if the cell is a flag or not
+   * @param isFlag
+   */
   setIsFlag(isFlag) {
     this.isFlag = isFlag;
   }
 
+  /**
+   * Get the number of adjacient mines
+   */
   getAdjMines() {
     return this.adjMines;
   }
 
+  /**
+   * Set the number of adjacient mines
+   * @param adjMines the new number of adjacient mines
+   */
   setAdjMines(adjMines) {
     this.adjMines = adjMines;
   }
 
+  /**
+   * Return true if the cell is revealed
+   */
   getIsRevealed() {
     return this.isRevealed;
   }
 
+  /**
+   * Set if the cell is revealed or not
+   * @param isRev
+   */
   setIsRevealed(isRev) {
     this.isRevealed = isRev;
   }
